de40d3c1c955fd50cbe1f90ea878e35742ec97c8
Ethan2022 <45220806+Ethan2022@users.noreply.github.com>
2018-11-20 22:57:11 -0500

    Add files via upload

---
+++ Goodbye163.java
@@ -0,0 +1,10 @@
+/**Simple code that prints "Goodbye COMP 163"
+ * @author emmckay@aggies.ncat.edu
+ */ 
+class Goodbye163 {
+  
+  public static void main(String[] args) {
+    
+    System.out.println("Goodbye COMP 163");
+  }
+}  
